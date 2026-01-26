c7c5e3e4d0297750ed751c8245a01dca47dd34b2
zbshi256 <zbshi256@qq.com>
2026-01-26 18:11:50 +0800

    Add dash background radius settings.

--- extension.js
+++ extension.js
@@ -10,6 +10,7 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
 const ICON_SIZES = [24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80];
 const PADDINGS = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32];
+const RADII = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32];
 
 const DashStyle = class {
     constructor(settings) {
@@ -28,10 +29,14 @@ const DashStyle = class {
         PADDINGS.forEach(padding => {
             this._uiGroup.remove_style_class_name(`dash-icon-size-padding${padding}`);
         });
+        RADII.forEach(radius => {
+            this._uiGroup.remove_style_class_name(`dash-icon-size-radius${radius}`);
+        });
 
         // Add new classes based on settings
         const iconSize = this._settings.get_int('icon-size');
         const padding = this._settings.get_int('dash-padding');
+        const radius = this._settings.get_int('dash-radius');
 
         // Find nearest valid size
         const validSize = ICON_SIZES.reduce((prev, curr) =>
@@ -43,8 +48,14 @@ const DashStyle = class {
             Math.abs(curr - padding) < Math.abs(prev - padding) ? curr : prev
         );
 
+        // Find nearest valid radius
+        const validRadius = RADII.reduce((prev, curr) =>
+            Math.abs(curr - radius) < Math.abs(prev - radius) ? curr : prev
+        );
+
         this._uiGroup.add_style_class_name(`dash-icon-size-icon${validSize}`);
         this._uiGroup.add_style_class_name(`dash-icon-size-padding${validPadding}`);
+        this._uiGroup.add_style_class_name(`dash-icon-size-radius${validRadius}`);
     }
 
     enable() {
@@ -63,6 +74,9 @@ const DashStyle = class {
         });
         PADDINGS.forEach(padding => {
             this._uiGroup.remove_style_class_name(`dash-icon-size-padding${padding}`);
+        });
+        RADII.forEach(radius => {
+            this._uiGroup.remove_style_class_name(`dash-icon-size-radius${radius}`);
         });
     }
 };
--- prefs.js
+++ prefs.js
@@ -6,6 +6,7 @@ import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/
 
 const ICON_SIZES = [24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80];
 const PADDINGS = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32];
+const RADII = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32];
 
 export default class DashIconSizePreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
@@ -49,5 +50,20 @@ export default class DashIconSizePreferences extends ExtensionPreferences {
         });
         group.add(paddingRow);
         window._settings.bind('dash-padding', paddingRow, 'value', Gio.SettingsBindFlags.DEFAULT);
+
+        // Border radius
+        const adjustmentRadius = new Gtk.Adjustment({
+            lower: RADII[0],
+            upper: RADII[RADII.length - 1],
+            step_increment: 2,
+        });
+
+        const radiusRow = new Adw.SpinRow({
+            title: 'Dash border radius (px)',
+            subtitle: `Border radius of the dash background. Available: ${RADII.join(', ')}px`,
+            adjustment: adjustmentRadius,
+        });
+        group.add(radiusRow);
+        window._settings.bind('dash-radius', radiusRow, 'value', Gio.SettingsBindFlags.DEFAULT);
     }
 }
